ab13b2d81a266938347a16e601380821cd7d1241
Uriel <ruriel51@gmail.com>
2024-05-13 14:29:49 -0300

    Correccion de errores de consola y agregado de efectos Desktop

---
+++ src/assets/Scroll-nav-bar.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const offset = 5 * 16;
+                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - offset;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+}, 1500);
+
+console.log('dentro del script de scroll')
